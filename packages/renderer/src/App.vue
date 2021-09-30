c3b141aeb4953cac57c760be0c5c7c6c2952b092
ljw1412 <506644791@qq.com>
2021-09-30 23:31:49 +0800

    chore: 字体预加载

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <router-view />
-  <app-font-loader></app-font-loader>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import AppFontLoader from './components/AppFontLoader.vue'
 import { ipcOn } from './utils/electron'
 
 export default defineComponent({
   name: 'App',
-  components: { AppFontLoader },
 
   created() {
     // 监听全局主题切换
